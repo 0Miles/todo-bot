3be2a14b483c242ecd0e8c30322ab9ef0143110b
Miles <swrev5741@gmail.com>
2022-06-28 08:36:05 +0800

    fix

--- src/events/todo.events.ts
+++ src/events/todo.events.ts
@@ -33,15 +33,18 @@ export class TodoEvents {
             const channel = await findTodoChannel(message.guildId ?? '', message.channelId);
             if (channel && !message.author.bot) {
                 if (message.mentions.members?.find(x => x.id === client.user?.id)) {
-                    const filteredMessage = await this.loadTodoMessage(message);
+                    const todoMessages = await this.loadTodoMessage(message);
 
-                    for (const eachMessage of filteredMessage) {
+                    for (const eachMessage of todoMessages) {
                         try {
-                            if (eachMessage.author.id === client.user?.id && eachMessage.type === 'REPLY') {
+                            if (eachMessage.reactions.cache.size !== 0 || (eachMessage.mentions.members?.size ?? 0) === 0) {
+                                await removeTodoMessage(eachMessage.guildId ?? '', eachMessage.channelId, eachMessage.id);
+                            } else if (eachMessage.author.id === client.user?.id && eachMessage.type === 'REPLY') {
                                 await eachMessage.delete();
                                 await removeTodoMessage(eachMessage.guildId ?? '', eachMessage.channelId, eachMessage.id);
                             } else if (eachMessage.hasThread) {
-                                await eachMessage.reply(eachMessage.content);
+                                const newMessage = await eachMessage.reply(eachMessage.content);
+                                await addTodoMessage(newMessage);
                             } else {
                                 const newMessage = await message.channel.send(eachMessage.content);
                                 await addTodoMessage(newMessage);
@@ -65,22 +68,26 @@ export class TodoEvents {
     @On("messageReactionAdd")
     async onMessageReactionAdd([message]: ArgsOf<"messageReactionAdd">, client: Client): Promise<void> {
         try {
-            if (message.message.type === 'REPLY') {
-                const refMessage = await message.message.channel.messages.fetch(message.message.reference?.messageId ?? '');
-                if (refMessage) {
-                    await refMessage.react('👍');
+            const channel = await findTodoChannel(message.message.guildId ?? '', message.message.channelId);
+            if (channel && (message.count ?? 0) > 0) {
+                if (message.message.type === 'REPLY') {
+                    const refMessage = await message.message.channel.messages.fetch(message.message.reference?.messageId ?? '');
+                    if (refMessage) {
+                        await refMessage.react('👍');
+                    }
                 }
+                removeTodoMessage(message.message.guildId ?? '', message.message.channelId, message.message.id);
             }
-            removeTodoMessage(message.message.guildId ?? '', message.message.channelId, message.message.id);
         } catch (ex) {
             console.error(ex);
         }
     }
-    
+
     @On("messageReactionRemove")
     async onmessageReactionRemove([message]: ArgsOf<"messageReactionRemove">, client: Client): Promise<void> {
         try {
-            if (message.count === 0) {
+            const channel = await findTodoChannel(message.message.guildId ?? '', message.message.channelId);
+            if (channel && message.count === 0) {
                 if (message.message.type === 'REPLY') {
                     const refMessage = await message.message.channel.messages.fetch(message.message.reference?.messageId ?? '');
                     if (refMessage) {
@@ -89,6 +96,7 @@ export class TodoEvents {
                 }
                 addTodoMessage(message.message as Message);
             }
+
         } catch (ex) {
             console.error(ex);
         }
